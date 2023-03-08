f71c5afc98c1c5c8c4657649640c5d4f08594454
flow6852 <flow6852math@gmail.com>
2023-03-08 20:07:21 +0900

    support url

--- denops/@ddu-sources/url.ts
+++ denops/@ddu-sources/url.ts
@@ -3,12 +3,20 @@ import {
   Item,
   SourceOptions,
 } from "https://deno.land/x/ddu_vim@v2.3.0/types.ts";
-import { Denops, fn } from "https://deno.land/x/ddu_vim@v2.3.0/deps.ts";
+import { Denops } from "https://deno.land/x/ddu_vim@v2.3.0/deps.ts";
 import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.3.2/file.ts";
-import { join } from "https://deno.land/std@0.177.0/path/mod.ts";
+import {
+  join,
+  relative,
+  resolve,
+} from "https://deno.land/std@0.177.0/path/mod.ts";
+import { abortable } from "https://deno.land/std@0.171.0/async/mod.ts";
 
 type Params = {
   src: string;
+  chunkSize: number;
+  ignoredDirectories: Array<string>;
+  expandSymbolicLink: boolean;
 };
 
 export class Source extends BaseSource<Params> {
@@ -21,40 +29,62 @@ export class Source extends BaseSource<Params> {
   }): ReadableStream<Item<ActionData>[]> {
     return new ReadableStream<Item<ActionData>[]>({
       async start(controller) {
-        const items: Item<ActionData>[] = [];
-        const cwd = await fn.getcwd(args.denops) as string;
-        const abspath: string = join(cwd, args.sourceParams.src);
-        let cmdarg = "-nHoE";
-        if ((await Deno.stat(await Deno.realPath(abspath))).isDirectory) {
-          cmdarg = "-nHRoE";
-        }
-        const proc = await Promise.resolve(
-          Deno.run({
-            cmd: [
-              "grep",
-              cmdarg,
-              "http(s?)://[0-9a-zA-Z?@=#+_&:/.%\-]+",
-              abspath,
-            ],
-            stdout: "piped",
-            stderr: "piped",
-          }),
-        );
-        for (
-          const output of new TextDecoder().decode(await proc.output()).split(
-            /\n/,
-          ).slice(0, -1)
-        ) {
-          const file: string = output.split(/:/)[0];
-          const lineNr: string = output.split(/:/)[1];
-          const url: string = output.split(/:/).slice(2).join(":");
-          // set action data
-          const action: ActionData = {
-            path: url,
-            text: url,
-            lineNr: Number(lineNr),
-          };
-          items.push({ word: file + ":" + url, action: action });
+        // initialize
+        let items: Item<ActionData>[] = [];
+        const abortController = new AbortController();
+
+        // select src
+        const regexpStr = "http(s?)://[0-9a-zA-Z?@=#+_&:/.%-]+";
+        if ((new RegExp(regexpStr)).test(args.sourceParams.src)) {
+          const response = await fetch(args.sourceParams.src);
+          if (!response.ok) {
+            console.error(
+              "response error: " + args.sourceParams.src + ": " +
+                response.statusText,
+            );
+            return;
+          }
+          const urls = getUrls(
+            await response.text(),
+            new RegExp(regexpStr, "g"),
+          );
+          for (const url of urls) {
+            items.push({
+              word: url,
+              action: {
+                path: url,
+                text: url,
+              },
+            });
+          }
+        } else {
+          const it = walkLocal(
+            resolve(args.sourceParams.src, args.sourceParams.src),
+            args.sourceParams.ignoredDirectories,
+            abortController.signal,
+            args.sourceParams.chunkSize,
+            args.sourceParams.expandSymbolicLink,
+            new RegExp(regexpStr, "g"),
+          );
+          let enqueueSize: number = args.sourceParams.chunkSize;
+          try {
+            for await (const chunk of it) {
+              items = items.concat(chunk);
+              if (items.length >= enqueueSize) {
+                enqueueSize = 10 * args.sourceParams.chunkSize;
+                controller.enqueue(items);
+                items = [];
+              }
+            }
+            if (items.length) {
+              controller.enqueue(items);
+            }
+          } catch (e: unknown) {
+            if (e instanceof DOMException) {
+              return;
+            }
+            console.error(e);
+          }
         }
         controller.enqueue(items);
         controller.close();
@@ -65,6 +95,80 @@ export class Source extends BaseSource<Params> {
   override params(): Params {
     return {
       src: ".",
+      chunkSize: 1000,
+      ignoredDirectories: [],
+      expandSymbolicLink: false,
     };
+  }
+}
+
+// File or Directory
+async function* walkLocal(
+  root: string,
+  ignoredDirectories: string[],
+  signal: AbortSignal,
+  chunkSize: number,
+  expandSymbolicLink: boolean,
+  regexp: RegExp,
+): AsyncGenerator<Item<ActionData>[]> {
+  const walkLocal = async function* (
+    dir: string,
+  ): AsyncGenerator<Item<ActionData>[]> {
+    let chunk: Item<ActionData>[] = [];
+    try {
+      for await (const entry of abortable(Deno.readDir(dir), signal)) {
+        const abspath = join(dir, entry.name);
+        if (
+          (await Deno.stat(await Deno.realPath(abspath))).isFile ||
+          (!expandSymbolicLink && !entry.isDirectory)
+        ) {
+          const raw = await Deno.readTextFile(abspath);
+          const urls = getUrls(raw, regexp);
+          for (const url of urls) {
+            const n = chunk.push({
+              word: relative(root, abspath) + ":" + url,
+              action: {
+                path: url,
+                text: url,
+              },
+            });
+            if (n >= chunkSize) {
+              yield chunk;
+              chunk = [];
+            }
+          }
+        } else if (ignoredDirectories.includes(entry.name)) {
+          continue;
+        } else if (
+          entry.isSymlink &&
+          (await Deno.stat(await Deno.realPath(abspath))).isDirectory &&
+          abspath.includes(await Deno.realPath(abspath))
+        ) {
+          continue;
+        } else {
+          yield* walkLocal(abspath);
+        }
+      }
+      if (chunk.length) {
+        yield chunk;
+      }
+    } catch (e: unknown) {
+      if (e instanceof Deno.errors.PermissionDenied) {
+        // Ignore this error
+        // See https://github.com/Shougo/ddu-source-file_rec/issues/2
+        return;
+      }
+      throw e;
+    }
+  };
+  yield* walkLocal(root);
+}
+
+function getUrls(src: string, regexp: RegExp) {
+  const match = src.match(regexp);
+  if (match) {
+    return match;
+  } else {
+    return [];
   }
 }
